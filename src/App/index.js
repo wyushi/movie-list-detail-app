293662da065ded0698d357a45bfa5c6ce2ba040a
Yushi Wang <jasper.ys.wang@gmail.com>
2019-01-28 23:52:09 -0800

    refactor App.js

---
+++ src/App/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+import styles from './styles'
+
+class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+      </View>
+    )
+  }
+}
+
+export default App
