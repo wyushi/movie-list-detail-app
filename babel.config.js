426f266090c23d86c85b8ae2ef9603faf8b397af
Yushi Wang <jasper.ys.wang@gmail.com>
2019-01-29 00:42:46 -0800

    add babel plugin module resolver

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,11 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        require.resolve('babel-plugin-module-resolver'), 
+        {root: ["./src/"]}
+      ]
+    ]
   };
 };
